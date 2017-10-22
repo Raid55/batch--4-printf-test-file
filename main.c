a7a08bb8de47b55d5a3bd441657fce107b50831d
Steven Garcia <stevengarciaacuna@gmail.com>
2017-10-22 08:54:22 -0700

    Update main.c

--- main.c
+++ main.c
@@ -14,21 +14,21 @@ int main(void)
 
 	/*---- Test cases for characters ----*/
 	c = 'h';
-	_printf("First letter of Holberton is %c, %d\n", c);
-
+	_printf("First letter in Holberton is %c\n", c); /* output: First letter in Holberton is h*/
+	printf("First letter in Holberton is %c\n", c);
 
 	/*---- Test cases for Numbers ----*/
 	number = 19912507;
-	_printf("%i, %d\n", number, number);
-
+	_printf("%i, %d\n", number, number); /* output: 19912507 19912507*/
+	printf("%i, %d\n", number, number);
 
 	/*---- Test cases for strings ----*/
 	str = "Holberton"
-	_printf("%s school\n", str);
-
+	_printf("%s school\n", str); /* output: Holberton school*/
+	printf("%s school\n", str);
 	/*---- Test cases for NULL -----*/
 	str = NULL;
-	_printf("%s school\n", str);
-
+	_printf("%s school\n", str); /*output: (null) school*/
+	printf("%s school\n", str);
 	return (0);
 }
